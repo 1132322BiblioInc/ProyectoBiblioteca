b986db83dc3abb3fbc672f010c977ba38f3e70de
DanielBastidas14 <dybastidas32@misena.edu.co>
2017-06-27 11:55:47 -0500

    Add files via upload

---
+++ Quinto Trimestre/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>biblio inc</title>
+<link rel="stylesheet" href="estilos.css"  type="text/css"/>
+
+<style type="text/css">
+<!--
+body {
+	background-image: url(66.jpg);
+}
+.Estilo3 {font-size: 10px}
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+#apDiv1 {
+	position: absolute;
+	width: 14px;
+	height: 15px;
+	z-index: 1;
+	left: 1667px;
+	top: 1105px;
+}
+#apDiv2 {
+	position: absolute;
+	width: 31px;
+	height: 22px;
+	z-index: 2;
+	left: -4px;
+	top: -30px;
+}
+-->
+</style></head>
+
+<body class="Estilo1" >
+
+<div><center>
+
+<table width="1516" height="1126" border="3" align="center" >
+  <tr align="center" valign="top">
+    <td height="283" colspan="3" > <iframe src="banner.php"  align="center" frameborder="0" scrolling="no" width="1467" height="300"> </iframe> </td>
+  </tr>
+  <tr align="center" valign="top">
+    <td height="215" colspan="3" > <iframe src="menu.php" align="center" frameborder="0" scrolling="no" width="1467" height="300" ></iframe></td>
+  </tr>
+  
+  <tr>
+    <td width="490" height="403" align="center" valign="middle"><table width="394" border="0">
+      <tr>
+        <td width="388"><img src="Imagen prueba/book.png" width="252" height="188" /></td>
+      </tr>
+      <tr>
+        <td><div align="right"><img src="Imagen prueba/libro (1).png" width="254" height="201" /></div></td>
+      </tr>
+      
+    </table>
+      </td>
+    <td width="490" height="403" align="center" valign="middle">
+	<center>
+	</center>	</td>
+    <td width="510" height="403" align="center" valign="middle"><center>
+    </center>   </td>
+  </tr>
+  <tr align="center" valign="top">
+    <td height="92" colspan="3" valign="bottom"> <iframe src="footer.php" align="center" frameborder="0" scrolling="no" width="1467" height="81"> </iframe>
+      <div id="apDiv1"><a href="acceso_admi.php"><img src="Imagen prueba/8.ico" width="21" height="18" /></a>
+        <div id="apDiv2"><a href="acceso_super_admi.php"><img src="Imagen prueba/super.png" width="25" height="23" /></a></div>
+      </div></td>
+  </tr>
+</table>
+</center>
+</div>
+<h1 align="center" class="Estilo1">&nbsp;</h1>
+
+
+</body>
+
+
+</html>
